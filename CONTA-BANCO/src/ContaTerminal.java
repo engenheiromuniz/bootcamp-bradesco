8f89a1580e2817b8e8459736b80a21ec52d04f08
Andre Muniz <engenheiro.muniz@outlook.com>
2025-08-24 17:16:33 -0300

    A classe ContaTerminal foi projetada conforme solicitação.

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Muniz");
+        Scanner sc = new Scanner(System.in);
+
+        Integer numero;
+        String  agencia;
+        String  nomeCliente;
+        Double  saldo;
+
+        System.out.println("- - - - - Início do Programa - - - - -\n");
+
+        System.out.print("\nPor favor digite o número da agência: ");        
+        agencia = sc.next();  
+        sc.nextLine();    
+        
+        System.out.print("\nDigite o número do usuário: ");
+        numero = sc.nextInt();
+                  
+
+        System.out.print("\nDigite o nome do cliente: ");
+        nomeCliente = sc.next();
+        sc.nextLine();
+
+        System.out.print("\nDigite o saldo: ");
+        saldo = sc.nextDouble();
+
+        System.out.print("\n\n- - - - Resposta do Sistema - - - - \n");
+        System.out.print("\nOlá "+nomeCliente
+                         +", obrigado por criar uma conta em nosso banco, "
+                         +" sua agência é a "+agencia
+                         +", conta "+numero
+                         +" e seu saldo de R$"+saldo+" já está disponível." );
+
+        sc.close();            
+
     }
 
 }
